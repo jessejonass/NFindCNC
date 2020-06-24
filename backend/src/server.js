81a2b7d66d4891c796d09fdd068c161247be9f0f
Jesse Jonas <jessejonas13@gmail.com>
2020-06-24 14:10:57 -0300

    feat: frontend da aplicação

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -15,7 +17,9 @@ mongoose.connect(process.env.MONGO_URL, {
 
 const routes = require('./routes');
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes); // Após o JSON
 
 app.listen(3333);
